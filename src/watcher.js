4544d135f0bca1c4acec04309d15cb3fd8664886
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2022-10-20 13:44:03 +0300

    Add handling new processStates, rename variable and correct i18n path in renderFeedback fn

--- src/watcher.js
+++ src/watcher.js
@@ -2,31 +2,39 @@ const handleProcessState = (elements, processState) => {
   const { form, input, submitBtn } = elements;
   switch (processState) {
     case 'filling':
-      submitBtn.disabled = false; // does it need?
+      submitBtn.disabled = false;
       break;
     case 'validating':
-      submitBtn.disabled = true; // does it need?
+      submitBtn.disabled = true;
       break;
     case 'validated':
-      submitBtn.disabled = false; // does it need?
+      submitBtn.disabled = false;
       input.style.border = null;
       form.reset();
       input.focus();
       break;
+    case 'loading':
+      submitBtn.disabled = true;
+      break;
+    case 'loaded':
+      submitBtn.disabled = false;
+      break;
     case 'invalidated':
       submitBtn.disabled = false;
       input.style.border = 'thick solid red';
+      break;
+    case 'network_error':
       break;
     default:
       throw new Error(`Unknown process state ${processState}`);
   }
 };
 
-const renderFeedback = (elements, error, i18nInstance) => {
+const renderFeedback = (elements, value, i18nInstance) => {
   const { feedback } = elements;
 
-  feedback.textContent = i18nInstance.t(error);
-  if (error === 'rssForm.feedback.success') {
+  feedback.textContent = i18nInstance.t(value);
+  if (value === 'rssForm.feedback.success') {
     feedback.classList.remove('text-danger');
     feedback.classList.add('text-success');
   } else {
