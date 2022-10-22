f4b35c9fc4fe06b1f293a62b5e03fa9a1971d90c
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2022-10-22 17:14:27 +0300

    Modify structure, add new text

--- locales/ru.js
+++ locales/ru.js
@@ -1,15 +1,13 @@
 export default {
   translation: {
-    rssForm: {
-      feedback: {
-        success: 'RSS успешно загружен',
-        errors: {
-          invalidUrl: 'Ссылка должна быть валидным URL',
-          duplicate: 'RSS уже существует',
-          network: 'Проблемы с интернетом. Попробуйте ещё раз',
-        },
+    feedback: {
+      success: 'RSS успешно загружен',
+      errors: {
+        invalidUrl: 'Ссылка должна быть валидным URL',
+        duplicate: 'RSS уже существует',
+        network: 'Проблемы с интернетом. Попробуйте ещё раз',
+        parser: 'Ресурс не содержит валидный RSS',
       },
     },
-
   },
 };
