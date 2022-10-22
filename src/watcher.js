0e77e8ede6d4efeba683aa416fdf4144de678860
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2022-10-22 17:30:17 +0300

    Modify functions:
    1. In 'handleProcessState' fn:
    1.1 Remove case 'validated': its role  will manage by 'loaded'
    1.2 Add new case 'parser_error'
    1.3 Add double quotes to default case
    
    2. In 'renderFeedback' fn: replace if-clauses with switch statement
    
    3. In default fn: add new cases and throwing error in default case

--- src/watcher.js
+++ src/watcher.js
@@ -4,29 +4,31 @@ const handleProcessState = (elements, processState) => {
     case 'filling':
       submitBtn.disabled = false;
       break;
+
     case 'validating':
       submitBtn.disabled = true;
       break;
-    case 'validated':
+
+    case 'loading':
+      submitBtn.disabled = true;
+      break;
+
+    case 'loaded':
       submitBtn.disabled = false;
       input.style.border = null;
       form.reset();
       input.focus();
       break;
-    case 'loading':
-      submitBtn.disabled = true;
-      break;
-    case 'loaded':
-      submitBtn.disabled = false;
-      break;
+
+    case 'parser_error':
+    case 'network_error':
     case 'invalidated':
       submitBtn.disabled = false;
       input.style.border = 'thick solid red';
       break;
-    case 'network_error':
-      break;
+
     default:
-      throw new Error(`Unknown process state ${processState}`);
+      throw new Error(`Unknown process state "${processState}"`);
   }
 };
 
@@ -34,12 +36,24 @@ const renderFeedback = (elements, value, i18nInstance) => {
   const { feedback } = elements;
 
   feedback.textContent = i18nInstance.t(value);
-  if (value === 'rssForm.feedback.success') {
-    feedback.classList.remove('text-danger');
-    feedback.classList.add('text-success');
-  } else {
-    feedback.classList.remove('text-success');
-    feedback.classList.add('text-danger');
+
+  switch (value) {
+    case 'feedback.success':
+      feedback.classList.remove('text-danger');
+      feedback.classList.add('text-success');
+      break;
+
+    case 'feedback.errors.duplicate':
+    case 'feedback.errors.invalidUrl':
+    case 'feedback.errors.network':
+    case 'feedback.errors.parser':
+      feedback.classList.remove('text-success');
+      feedback.classList.add('text-danger');
+      break;
+
+    default:
+      console.log('Oops...');
+      throw new Error(`Unknown feedback code "${value}"`);
   }
 };
 
@@ -48,10 +62,25 @@ export default (elements, i18nInstance) => (path, value) => {
     case 'rssForm.processState':
       handleProcessState(elements, value);
       break;
+
     case 'rssForm.feedback':
       renderFeedback(elements, value, i18nInstance);
       break;
-    default:
+
+    case 'watchedState.data.feeds':
+      // handling code
       break;
+
+    case 'watchedState.data.posts':
+      // handling code
+      break;
+
+    case 'rssForm.link':
+    case 'urls':
+    case 'rssForm.valid':
+      break;
+
+    default:
+      throw new Error(`Unwatched path ${path}`);
   }
 };
